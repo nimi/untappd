a0cd4e6f41f8e7caf207a471a49e949708d62e12
Nicholas Mitchell <mitchellnicholasl@gmail.com>
2016-03-14 00:03:39 -0400

    Add pub api wrapper

---
+++ lib/untappd/pub.ex
@@ -0,0 +1,25 @@
+defmodule Untappd.Pub do
+  import Untappd
+  alias Untappd.Client
+
+  @doc """
+    The 'pub' local activity
+  """
+
+  @doc """
+  lat (float, required) - The latitude of the query
+  lng (float, required) - The longitude of the query
+  max_id (int, optional) - The checkin ID that you want the results to start with
+  min_id (int, optional) - Returns only checkins that are newer than this value
+  limit (int, optional) - The number of results to return, max of 25, default is 25
+  radius (int, optional) - The max radius you would like the check-ins to start within,
+  max of 25, default is 25
+  dist_pref (string, optional) - If you want the results returned in miles or km. Available
+  options: "m", or "km". Default is "m"
+  """
+  @spec local(map, Client.t) :: Untappd.response
+  def local(params, client \\ %Client{}) do
+    get "thepub/local", client, params
+  end
+
+end
